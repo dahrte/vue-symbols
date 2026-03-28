bb1d615c075bf990977807599aaaf1c3fc4d86f6
dahrte <robert0d3@proton.me>
2026-03-28 16:54:40 -0600

    feat(www): add pattern divider after header and before footer

--- www/src/App.vue
+++ www/src/App.vue
@@ -1,12 +1,24 @@
 <script setup lang="ts">
+import Container from '@/components/Container.vue'
 import Footer from '@/components/Footer.vue'
 import Header from '@/components/Header.vue'
+import PatternDivider from '@/components/PatternDivider.vue'
 import { TooltipProvider } from '@/ui/tooltip'
 
 <template>
   <TooltipProvider>
     <Header />
+
+    <Container class="w-full px-0">
+      <PatternDivider class="h-8 w-full border-b" />
+    </Container>
+
     <RouterView />
+
+    <Container class="w-full px-0">
+      <PatternDivider class="h-8 w-full border-t" />
+    </Container>
+
     <Footer />
   </TooltipProvider>
 </template>
