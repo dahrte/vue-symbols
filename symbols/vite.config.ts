08b099a7e843e7a7db318dbbc366745f336c676d
dahrte <robert0d3@proton.me>
2026-04-01 18:18:53 -0600

    chore: update packages scripts and task definitions

--- symbols/vite.config.ts
+++ symbols/vite.config.ts
@@ -1,13 +1,22 @@
 import { defineConfig } from 'vite-plus'
 
 export default defineConfig({
+  run: {
+    tasks: {
+      'build:components': {
+        command: 'node scripts/build-components.ts',
+      },
+      build: {
+        command: 'vp pack',
+        dependsOn: ['build:components'],
+      },
+    },
+  },
   pack: {
     deps: {
       neverBundle: ['vue'],
     },
-    dts: {
-      vue: true,
-    },
+    dts: true,
     entry: {
       'files/index': 'src/files.ts',
       'folders/index': 'src/folders.ts',
